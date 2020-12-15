23b7966ce4c61c65885839b78d5c31081c386002
Александр <alexandrublev@yandex.ru>
2020-12-16 00:29:32 +0300

    last

---
+++ login.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#link-login').addEventListener('click', function() {
+        document.querySelector('#login').classList.toggle('not_shown')
+    })
+})
